ef39236a790fdb51ce0ba66971fbc1558157f8dc
vitorfebrus <vitor.sampaio@capgemini.com>
2020-07-29 17:59:26 -0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 5000
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
     console.log('Example app listening on port 3000!');
 });
