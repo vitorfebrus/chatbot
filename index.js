0cc1f3b8d0f6fa51a6baeee6417f417d920fa809
vitorfebrus <vitor.sampaio@capgemini.com>
2020-07-30 11:17:39 -0300

    configurando chatbot

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 5000
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, function() {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 5000!');
 });
